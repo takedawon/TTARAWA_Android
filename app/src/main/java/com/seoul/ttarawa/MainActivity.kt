6d632accd7dcd4edc641015f216d1dad714a63ea
takedawon <takedawon@naver.com>
2019-07-16 11:34:38 +0900

    Firebase 연동

--- app/src/main/java/com/seoul/ttarawa/MainActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/MainActivity.kt
@@ -2,11 +2,35 @@ package com.seoul.ttarawa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.pm.PackageManager
+import android.widget.Toast
+import android.provider.SyncStateContract.Helpers.update
+import android.content.pm.PackageInfo
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+            val info = packageManager.getPackageInfo("com.seoul.ttarawa", PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val str = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                Log.d("KeyHash:", str)
+                Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
     }
 }
