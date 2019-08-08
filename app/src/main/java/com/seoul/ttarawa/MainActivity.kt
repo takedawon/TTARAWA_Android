7bed34d463fa7a1440ccdbaba967ef78e8db5256
takedawon <takedawon@naver.com>
2019-08-09 02:07:04 +0900

    BottomNavigationBar 추가

--- app/src/main/java/com/seoul/ttarawa/MainActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.seoul.ttarawa
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.pm.PackageManager
-import android.widget.Toast
-import android.provider.SyncStateContract.Helpers.update
-import android.content.pm.PackageInfo
-import android.util.Base64
-import android.util.Log
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    val home = HomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, home)
+            .commitAllowingStateLoss() // 첫 화면 지정 HomeFragment
+
+        bottom_navigation_bar.setOnNavigationItemSelectedListener(this)
+
+
+        /*
+         * 해시키 값 출력 해주는 코드
+         * 주석처리. 필요 시 해제해서 사용할 것.
+         * try {
             val info = packageManager.getPackageInfo("com.seoul.ttarawa", PackageManager.GET_SIGNATURES)
             for (signature in info.signatures) {
                 val md = MessageDigest.getInstance("SHA")
@@ -31,6 +39,25 @@ class MainActivity : AppCompatActivity() {
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
+        */
+    }
 
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.bottom_home -> {
+                val homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_layout, homeFragment).commit()
+            }
+            R.id.bottom_menu -> {
+                val menuFragment = MenuFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_layout, menuFragment).commit()
+            }
+            R.id.bottom_info -> {
+                val infoFragment = InfoFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_layout, infoFragment).commit()
+            }
+        }
+        return true
     }
 }
+
