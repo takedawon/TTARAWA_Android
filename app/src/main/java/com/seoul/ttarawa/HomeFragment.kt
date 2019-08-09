092fdc613e098321bd0cc7df1be43efff1ce4dfd
takedawon <takedawon@naver.com>
2019-08-09 18:58:48 +0900

    KAKAO MAP API 사용

--- app/src/main/java/com/seoul/ttarawa/HomeFragment.kt
+++ app/src/main/java/com/seoul/ttarawa/HomeFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
+import net.daum.android.map.MapView
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val mapView = MapView(activity)
+        val mapViewContainer = map_view
+        mapViewContainer.addView(mapView)
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
