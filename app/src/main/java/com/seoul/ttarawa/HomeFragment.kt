c5ffce14252c746c039c1843fe45cccb5968e2c1
takedawon <takedawon@naver.com>
2019-08-11 22:58:56 +0900

    KAKAO MAP HD TILE 사용 및 NO ACTION BAR 사용

--- app/src/main/java/com/seoul/ttarawa/HomeFragment.kt
+++ app/src/main/java/com/seoul/ttarawa/HomeFragment.kt
@@ -1,12 +1,10 @@
 package com.seoul.ttarawa
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import net.daum.mf.map.api.MapView
 
@@ -15,6 +13,8 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val mapView = MapView(activity)
         val mapViewContainer = view.map_view
+
+        mapView.isHDMapTileEnabled = true
         mapViewContainer.addView(mapView)
 
 
