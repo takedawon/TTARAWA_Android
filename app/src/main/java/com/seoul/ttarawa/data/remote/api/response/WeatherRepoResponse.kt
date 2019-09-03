0756d245eca11a20c3177cde7f68673cd7b1f8e9
takedawon <takedawon@naver.com>
2019-09-03 21:45:22 +0900

    WeatherRepoResponse 추가

---
+++ app/src/main/java/com/seoul/ttarawa/data/remote/api/response/WeatherRepoResponse.kt
@@ -0,0 +1,58 @@
+package com.seoul.ttarawa.data.remote.api
+
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherRepoResponse(
+    @SerializedName("response")
+    val response: Response
+) {
+    data class Response(
+        @SerializedName("body")
+        val body: Body,
+        @SerializedName("header")
+        val header: Header
+    ) {
+        data class Body(
+            @SerializedName("items")
+            val items: Items,
+            @SerializedName("numOfRows")
+            val numOfRows: Int,
+            @SerializedName("pageNo")
+            val pageNo: Int,
+            @SerializedName("totalCount")
+            val totalCount: Int
+        ) {
+            data class Items(
+                @SerializedName("item")
+                val item: List<Item>
+            ) {
+                data class Item(
+                    @SerializedName("baseDate")
+                    val baseDate: Int,
+                    @SerializedName("baseTime")
+                    val baseTime: Int,
+                    @SerializedName("category")
+                    val category: String,
+                    @SerializedName("fcstDate")
+                    val fcstDate: Int,
+                    @SerializedName("fcstTime")
+                    val fcstTime: Int,
+                    @SerializedName("fcstValue")
+                    val fcstValue: Int,
+                    @SerializedName("nx")
+                    val nx: Int,
+                    @SerializedName("ny")
+                    val ny: Int
+                )
+            }
+        }
+
+        data class Header(
+            @SerializedName("resultCode")
+            val resultCode: String,
+            @SerializedName("resultMsg")
+            val resultMsg: String
+        )
+    }
+}
