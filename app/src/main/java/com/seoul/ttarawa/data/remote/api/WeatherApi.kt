1e7bbbb442926d5ec58e29ac03c6e8bab9c79a60
Daeyeon Kim <improve777@gmail.com>
2019-09-03 23:05:17 +0900

    WeatherApi 수정

--- app/src/main/java/com/seoul/ttarawa/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/seoul/ttarawa/data/remote/api/WeatherApi.kt
@@ -1,22 +1,22 @@
 package com.seoul.ttarawa.data.remote.api
 
-import retrofit2.Call
+import androidx.annotation.IntRange
+import com.seoul.ttarawa.data.remote.response.WeatherResponse
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val BASE_URL = "http://newsky2.kma.go.kr/"
-
 interface WeatherApi {
 
-    @GET("/service/SecndSrtpdFrcstInfoService2/ForecastTimeData")
-    abstract fun getWeather(
+    @GET("service/SecndSrtpdFrcstInfoService2/ForecastTimeData")
+    fun getWeather(
         @Query("serviceKey") serviceKey: String,
         @Query("base_date") baseDate: String,
         @Query("base_time") baseTime: String,
-        @Query("nx") nx: String,
-        @Query("ny") ny: String,
-        @Query("numOfRows") numOfRows: String,
-        @Query("pageNo") pageNo: String,
-        @Query("_type") type: String
-    ): Call<WeatherRepoResponse>
+        @Query("nx") nx: Int,
+        @Query("ny") ny: Int,
+        @IntRange(from = 1, to = 99) @Query("numOfRows") numOfRows: Int,
+        @IntRange(from = 1) @Query("pageNo") pageNo: Int,
+        @Query("_type") type: String = "json"
+    ): Single<WeatherResponse>
 }
