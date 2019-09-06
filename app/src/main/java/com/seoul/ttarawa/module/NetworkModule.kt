a95e5bda8b6be4c4b619c0f9927755c95caabafe
Daeyeon Kim <improve777@gmail.com>
2019-09-06 23:16:24 +0900

    NetworkModule 메서드 어노테이션 추가

--- app/src/main/java/com/seoul/ttarawa/module/NetworkModule.kt
+++ app/src/main/java/com/seoul/ttarawa/module/NetworkModule.kt
@@ -1,7 +1,6 @@
 package com.seoul.ttarawa.module
 
 import com.seoul.ttarawa.BuildConfig
-import com.seoul.ttarawa.data.remote.api.SeoulApi
 import com.seoul.ttarawa.data.remote.api.WeatherApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -10,10 +9,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
 
-    val seoulApi = createRetrofit("").create(SeoulApi::class.java)
+    // val seoulApi: SeoulApi = createRetrofit("").create(SeoulApi::class.java)
 
-    val weatherApi = createRetrofit("http://newsky2.kma.go.kr/").create(WeatherApi::class.java)
+    val weatherApi: WeatherApi = createRetrofit("http://newsky2.kma.go.kr/").create(WeatherApi::class.java)
 
+    @JvmStatic
     private fun createRetrofit(uri: String) =
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -21,11 +21,13 @@ object NetworkModule {
             .baseUrl(uri)
             .build()
 
+    @JvmStatic
     private fun createOkHttpClient() =
         OkHttpClient.Builder()
             .addInterceptor(createLoggingInterceptor())
             .build()
 
+    @JvmStatic
     private fun createLoggingInterceptor() =
         HttpLoggingInterceptor().apply {
             level = if (BuildConfig.DEBUG) {
