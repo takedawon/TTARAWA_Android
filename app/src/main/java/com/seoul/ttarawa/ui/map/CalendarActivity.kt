7761fe1e214ae0069f0a2dfc7a73b73447f03253
Daeyeon Kim <improve777@gmail.com>
2019-09-06 21:45:56 +0900

    CalendarFragment 를 Activity 로 변경

--- app/src/main/java/com/seoul/ttarawa/ui/map/CalendarActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/ui/map/CalendarActivity.kt
@@ -2,22 +2,21 @@ package com.seoul.ttarawa.ui.map
 
 import android.os.Bundle
 import com.seoul.ttarawa.R
-import com.seoul.ttarawa.base.BaseFragment
-import com.seoul.ttarawa.databinding.FragmentCalendarBinding
+import com.seoul.ttarawa.base.BaseActivity
+import com.seoul.ttarawa.databinding.ActivityCalendarBinding
 import com.seoul.ttarawa.ext.click
-import com.seoul.ttarawa.ext.replaceInFragment
-import org.jetbrains.anko.support.v4.toast
+import org.jetbrains.anko.toast
 
 /**
  * 달력 화면
  * 날짜 선택 시 검색 화면으로 이동
  */
-class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
-    R.layout.fragment_calendar
+class CalendarActivity : BaseActivity<ActivityCalendarBinding>(
+    R.layout.activity_calendar
 ) {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         initView()
     }
@@ -25,7 +24,7 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
     override fun initView() {
         bind {
             btnCalBack click {
-                activity?.onBackPressed()
+                onBackPressed()
                 // overridePendingTransition(R.anim.not_move_activity, R.anim.rightout_activity)
             }
 
@@ -34,14 +33,8 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(
                     year.toString() + "년" + (month + 1).toString() + "월" + dayOfMonth.toString() + "일을 선택하셨습니다."
                 toast(msg)
 
-                replaceInFragment(R.id.container_plan, SearchFragment.newInstance())
+                // todo 패스 액티비티 이동
             }
-        }
-    }
-
-    companion object {
-        fun newInstance(): CalendarFragment {
-            return CalendarFragment()
         }
     }
 }
