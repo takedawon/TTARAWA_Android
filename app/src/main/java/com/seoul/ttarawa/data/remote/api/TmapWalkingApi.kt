156b4eec093983627c93a809b5738e54b8471ea8
takedawon <takedawon@naver.com>
2019-09-09 16:57:41 +0900

    TmapWalkingApi 추가

---
+++ app/src/main/java/com/seoul/ttarawa/data/remote/api/TmapWalkingApi.kt
@@ -0,0 +1,21 @@
+package com.seoul.ttarawa.data.remote.api
+
+import com.seoul.ttarawa.data.remote.response.WeatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface TmapWalkingApi {
+    @GET("tmap/routes/pedestrian")
+    fun getWalkingPath(
+        @Header("appKey") appKey:String,
+        @Query("version") version: String,
+        @Query("startX") startX: String,
+        @Query("startY") startY: String,
+        @Query("endX") endX: String,
+        @Query("endY") endY: String,
+        @Query("startName") startName: String,
+        @Query("endName") endName: String
+    ): Call<WeatherResponse>
+}
