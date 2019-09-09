ce09af291d7114cb97063fb0a748d3ddd7f8f14b
takedawon <takedawon@naver.com>
2019-09-09 16:20:16 +0900

    CalendarView 오늘 날짜 클릭 시 다음 액티비티로 넘어가지 않던 버그 수정

--- app/src/main/java/com/seoul/ttarawa/ui/map/CalendarActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/ui/map/CalendarActivity.kt
@@ -38,6 +38,7 @@ class CalendarActivity : BaseActivity<ActivityCalendarBinding>(
                     set(Calendar.YEAR, year)
                     set(Calendar.MONTH, month)
                     set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    roll(Calendar.MINUTE, +1)
                 }
 
                 if (isBeforeDay(chooseCalendar)) {
