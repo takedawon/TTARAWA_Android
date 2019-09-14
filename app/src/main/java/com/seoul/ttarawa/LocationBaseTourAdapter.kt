f769698212a7711115a6342a51929f4b622bca95
takedawon <takedawon@naver.com>
2019-09-14 13:25:05 +0900

    LocationBaseTourAdapter 추가

---
+++ app/src/main/java/com/seoul/ttarawa/LocationBaseTourAdapter.kt
@@ -0,0 +1,48 @@
+package com.seoul.ttarawa
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class LocationBaseTourAdapter : RecyclerView.Adapter<LocationBaseTourAdapter.ViewHolder>() {
+    val items: ArrayList<LocationTourModel> = ArrayList<LocationTourModel>()
+
+    fun addItem(item: LocationTourModel) {
+        items.add(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(parent.context).
+            inflate(R.layout.custom_tour_list, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item: LocationTourModel = items[position]
+        holder.setItem(item)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageTourPhoto = itemView?.findViewById<ImageView>(R.id.img_tour_photo)
+        private val txtTitle = itemView?.findViewById<TextView>(R.id.txt_title)
+        private val txtAddress = itemView?.findViewById<TextView>(R.id.txt_address)
+        private val txtDistance = itemView?.findViewById<TextView>(R.id.txt_distance)
+
+        fun setItem(model: LocationTourModel) {
+            Glide.with(itemView).load(model.imgUrl).into(imageTourPhoto)
+            txtTitle.text = model.title
+            txtDistance.text = model.distance
+            txtAddress.text = model.address
+        }
+
+    }
+}
