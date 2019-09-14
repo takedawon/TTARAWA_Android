8c8d2441964cc026ea7abc2fe8ab4e9855962338
takedawon <takedawon@naver.com>
2019-09-14 11:34:48 +0900

    LocationBaseTourApi 추가

---
+++ app/src/main/java/com/seoul/ttarawa/data/remote/api/LocationBaseTourApi.kt
@@ -0,0 +1,24 @@
+package com.seoul.ttarawa.data.remote.api
+
+import com.seoul.ttarawa.data.remote.response.LocationBaseTourResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Call
+
+interface LocationBaseTourApi {
+    @GET("openapi/service/rest/KorService/locationBasedList")
+    fun getLocationBaseTour(
+        @Query("ServiceKey") serviceKey: String,
+        @Query("numOfRows") numOfRows: Int,
+        @Query("pageNo") pageNo: Int,
+        @Query("MobileOS") mobileOS: String,
+        @Query("MobileApp") mobileApp: String,
+        @Query("arrange") arrange: String,
+        @Query("contentTypeId") contentTypeId: Int,
+        @Query("mapX") mapX: String,
+        @Query("mapY") mapY: String,
+        @Query("radius") radius: Int,
+        @Query("listYN") listYN: String,
+        @Query("_type") type: String = "json"
+    ): Call<LocationBaseTourResponse>
+}
