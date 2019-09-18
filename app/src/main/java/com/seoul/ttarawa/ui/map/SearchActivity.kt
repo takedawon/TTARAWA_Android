f045857fcbaea7cabbc9a79ede51dc473a51ed42
Daeyeon Kim <improve777@gmail.com>
2019-09-18 13:10:47 +0900

    SearchActivity.kt 디자인 수정

--- app/src/main/java/com/seoul/ttarawa/ui/map/SearchActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/ui/map/SearchActivity.kt
@@ -1,10 +1,14 @@
 package com.seoul.ttarawa.ui.map
 
+import android.graphics.Point
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.seoul.ttarawa.R
 import com.seoul.ttarawa.base.BaseActivity
 import com.seoul.ttarawa.databinding.ActivitySearchBinding
-import com.seoul.ttarawa.ext.click
+import org.jetbrains.anko.dip
 
 /**
  * 검색 화면
@@ -15,6 +19,8 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(
     R.layout.activity_search
 ) {
 
+    private var bottomSheetBehavior: BottomSheetBehavior<View>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
@@ -22,8 +28,47 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(
 
     override fun initView() {
         bind {
-            filterIcon click {
+            setSupportActionBar(tbSearch)
+            supportActionBar?.title = getString(R.string.path_actionbar_title)
+
+            llSearchBottomSheet.layoutParams.height = getHeightWithoutActionBarSize()
+            bottomSheetBehavior = BottomSheetBehavior.from(binding.llSearchBottomSheet)
+        }
+    }
+
+    private fun getHeightWithoutActionBarSize(): Int {
+        val point = Point()
+        this@SearchActivity.windowManager.defaultDisplay.getSize(point)
+        return point.y - dip(56)
+    }
+
+    override fun onBackPressed() {
+        if (bottomSheetBehavior?.state == BottomSheetBehavior.STATE_EXPANDED) {
+            bottomSheetBehavior?.state = BottomSheetBehavior.STATE_COLLAPSED
+            return
+        }
+        super.onBackPressed()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            when (it.itemId) {
+                android.R.id.home -> {
+                    bottomSheetBehavior?.run {
+                        state = if (state == BottomSheetBehavior.STATE_EXPANDED) {
+                            BottomSheetBehavior.STATE_COLLAPSED
+
+                        } else {
+                            BottomSheetBehavior.STATE_EXPANDED
+                        }
+                        return true
+                    }
+                }
+                else -> {
+                    /*ignored*/
+                }
             }
         }
+        return super.onOptionsItemSelected(item)
     }
 }
