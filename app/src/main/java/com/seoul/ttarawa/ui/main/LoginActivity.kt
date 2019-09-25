0e4cd177774ad45d496287844b063d614e002550
takedawon <takedawon@naver.com>
2019-09-26 01:08:57 +0900

    JoinActivity 디자인 수정

--- app/src/main/java/com/seoul/ttarawa/ui/main/LoginActivity.kt
+++ app/src/main/java/com/seoul/ttarawa/ui/main/LoginActivity.kt
@@ -3,11 +3,20 @@ package com.seoul.ttarawa.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.seoul.ttarawa.R
+import com.seoul.ttarawa.base.BaseActivity
+import com.seoul.ttarawa.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        initView()
     }
+
+    override fun initView() {
+
+    }
+
 }
